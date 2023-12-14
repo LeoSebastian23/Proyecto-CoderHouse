017234420a80a1015a6f5e1cf48c213ab9c8fbaf
LeoSebastian23 <leosebastiang19@gmail.com>
2023-12-13 21:58:42 -0300

    Update

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -2,13 +2,13 @@ import { FlatList, StyleSheet, Text, View } from 'react-native'
 import categories from "../Data/categories.json"
 import CategoryItem from './CategoryItems'
 
-const Categories = () => {
+const Categories = ({setCategorySelected}) => {
   return (
     <FlatList
         style={styles.container}
         data={categories}
         keyExtractor={item => item}
-        renderItem={({item}) => <CategoryItem category={item}/>}
+        renderItem={({item}) => <CategoryItem setCategorySelected={setCategorySelected} category={item}/>}
     />
   )
 }
